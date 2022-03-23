2294f36dbe3bc81cc71e220a81c73ee31a9313d7
Damir Mansurov <d.mansurov@mandarin.io>
2022-03-23 20:13:17 +0300

    crud for city

--- ClientsTable/Controllers/CityController.cs
+++ ClientsTable/Controllers/CityController.cs
@@ -31,12 +31,41 @@ namespace ClientsTable.Controllers
             return Ok(cities);
         }
 
+        [HttpGet("{cityId}")]
+        public async Task<IActionResult> GetById(int cityId)
+        {
+            var entity = await _context.Cities.FirstOrDefaultAsync(x => x.Id == cityId);
+
+            if (entity == null) return NotFound();
+            
+            return Ok(entity);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Create(CityEntity cityEntity)
         {
             var result = await _context.Cities.AddAsync(cityEntity);
             await _context.SaveChangesAsync();
             return Ok(result.Entity.Id);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CityEntity cityEntity)
+        {
+            _context.Cities.Update(cityEntity);
+            await _context.SaveChangesAsync();
+            
+            return Ok();
+        }
+
+        [HttpDelete("{cityId}")]
+        public async Task<IActionResult> Delete(int cityId)
+        {
+            var entity = await _context.Cities.FirstOrDefaultAsync(x=> x.Id == cityId);
+            _context.Cities.Remove(entity);
+            await _context.SaveChangesAsync();
+            
+            return Ok();
         }
     }
 }
